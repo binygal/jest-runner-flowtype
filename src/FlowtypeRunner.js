8e34bbe81962a972b77503d08cecf7ac0a1e39a4
Binaymin Galinsky <binygal@gmail.com>
2018-05-29 07:56:42 +0300

    add colors to jest flowtype output

--- src/FlowtypeRunner.js
+++ src/FlowtypeRunner.js
@@ -1,5 +1,6 @@
 const { exec } = require('child_process');
 const path = require('path');
+const stripAnsi = require('strip-ansi');
 const { pass, fail } = require('create-jest-runner');
 
 class FlowtypeRunner {
@@ -10,36 +11,40 @@ class FlowtypeRunner {
   runTests(tests, watcher, onStart, onResult, onFailure, options) {
     const start = +new Date();
     return new Promise((resolve) => {
-      exec('flow', { stdio: 'ignore', cwd: this.globalConfig.rootDir }, (err, stdout) => {
-        const errors = stdout.split('Error');
-        const errorsPerFile = errors.reduce((previous, current) => {
-          const firstErrorLine = current.split('\n')[0];
-          const fileNameMatcher = firstErrorLine.match(/(\.{1,2}|\/)?([A-z]|\/|-)*\.js(x?)/);
-          if (fileNameMatcher) {
-            const fileName = path.join(this.globalConfig.rootDir, fileNameMatcher[0]);
-            const errorMessage = current.substring(current.indexOf('\n') + 1);
-            if (!previous[fileName]) {
-              previous[fileName] = [];
+      exec(
+        'flow --color=always',
+        { stdio: 'ignore', cwd: this.globalConfig.rootDir },
+        (err, stdout) => {
+          const errors = stdout.split('Error');
+          const errorsPerFile = errors.reduce((previous, current) => {
+            const firstErrorLine = current.split('\n')[0];
+            const fileNameMatcher = stripAnsi(firstErrorLine).match(/(\.{1,2}|\/)?([A-z]|\/|-)*\.js(x?)/);
+            if (fileNameMatcher) {
+              const fileName = path.join(this.globalConfig.rootDir, fileNameMatcher[0]);
+              const errorMessage = current.substring(current.indexOf('\n') + 1);
+              if (!previous[fileName]) {
+                previous[fileName] = [];
+              }
+              previous[fileName].push(errorMessage);
             }
-            previous[fileName].push(errorMessage);
-          }
-          return previous;
-        }, {});
-        tests.forEach((t) => {
-          let testResults;
-          if (errorsPerFile[t.path]) {
-            testResults = fail({
-              start,
-              end: +new Date(),
-              test: { path: t.path, errorMessage: errorsPerFile[t.path] },
-            });
-          } else {
-            testResults = pass({ start, end: +new Date(), test: { path: t.path } });
-          }
-          onResult(t, testResults);
-        });
-        resolve();
-      });
+            return previous;
+          }, {});
+          tests.forEach((t) => {
+            let testResults;
+            if (errorsPerFile[t.path]) {
+              testResults = fail({
+                start,
+                end: +new Date(),
+                test: { path: t.path, errorMessage: errorsPerFile[t.path] },
+              });
+            } else {
+              testResults = pass({ start, end: +new Date(), test: { path: t.path } });
+            }
+            onResult(t, testResults);
+          });
+          resolve();
+        },
+      );
     });
   }
 }
